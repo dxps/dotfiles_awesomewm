686aff7adec35be76b66ed844a2b0625172d0ebc
dxps <marius.ileana@gmail.com>
2021-05-02 21:28:09 +0300

    (cpu_widget) popup width 390

--- dot_config___awesome/awesome-wm-widgets/cpu-widget/cpu-widget.lua
+++ dot_config___awesome/awesome-wm-widgets/cpu-widget/cpu-widget.lua
@@ -129,7 +129,7 @@ local function worker(user_args)
         shape = gears.shape.rounded_rect,
         border_width = 1,
         border_color = beautiful.bg_normal,
-        maximum_width = 380,
+        maximum_width = 390,
         offset = { y = 15 },
         widget = {}
     }
